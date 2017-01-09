3e9e3bdd68ccb5f6076aae1b3dcb6ce0ea8f6fa7
dip <dmytro.piskun@gmail.com>
2017-01-09 23:26:15 +0200

    Removed unused usings

--- UnitTests.Backup.REST.Web.API/Controllers/BackupControllerTests.cs
+++ UnitTests.Backup.REST.Web.API/Controllers/BackupControllerTests.cs
@@ -1,10 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Backup.REST.Web.API.Controllers;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UnitTests.Backup.REST.Web.API.Controllers
 {
