4393da672bc7e73fc2cc17fdc041897c45ff1637
dip <dmytro.piskun@gmail.com>
2017-01-10 12:13:12 +0200

    Added client BL unit tests project.

---
+++ UnitTests.Backup.Client.BL/BackupWorkerTests.cs
@@ -0,0 +1,25 @@
+﻿using Backup.Client.BL;
+using Backup.Common.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTests.Backup.Client.BL
+{
+    [TestClass]
+    public class BackupWorkerTests
+    {
+        [TestMethod]
+        public void DoBackupTest()
+        {
+            // Arrange
+            var configMock = new Mock<IBackupConfig>();
+            var backupWorker = new BackupWorker(configMock.Object);
+
+            // Act
+            backupWorker.DoBackup();
+
+            // Assert
+            configMock.VerifyGet(c => c.SourceFolderPath, Times.AtLeastOnce());
+        }
+    }
+}
--- UnitTests.Backup.Client.BL/Properties/AssemblyInfo.cs
+++ UnitTests.Backup.Client.BL/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Backup.Client.BL")]
+[assembly: AssemblyTitle("UnitTests.Backup.Client.BL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Backup.Client.BL")]
+[assembly: AssemblyProduct("UnitTests.Backup.Client.BL")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86c46dff-298f-417b-a1e2-42de93c1fa9a")]
+[assembly: Guid("d10aacf5-cb57-49b3-80fd-ec769139931f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -34,4 +34,3 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("UnitTests.Backup.Client.BL")]
