73981976db78c094dc4ab8d665c6bb6411124d9a
dip <dmytro.piskun@gmail.com>
2017-01-11 21:19:26 +0200

    Added ScheduledBackup DTO.

---
+++ Backup.Common/DTO/ScheduledBackup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Backup.Common.DTO
+{
+    /// <summary>
+    ///     Holds information about scheduled backup.
+    /// </summary>
+    [Serializable]
+    public class ScheduledBackup
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="ScheduledBackup" /> class.
+        /// </summary>
+        /// <param name="scheduledDateTime">The scheduled date time.</param>
+        /// <param name="backupConfig">The backup configuration.</param>
+        public ScheduledBackup(DateTime scheduledDateTime, BackupConfig backupConfig)
+        {
+            ScheduledDateTime = scheduledDateTime;
+            BackupConfig = backupConfig;
+        }
+
+        /// <summary>
+        ///     Gets or sets the scheduled date time.
+        /// </summary>
+        /// <value>
+        ///     The scheduled date time.
+        /// </value>
+        public DateTime ScheduledDateTime { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the backup configuration.
+        /// </summary>
+        /// <value>
+        ///     The backup configuration.
+        /// </value>
+        public BackupConfig BackupConfig { get; set; }
+    }
+}
