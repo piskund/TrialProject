9220bd2355a7396bc6510c0244bf817e15623ddf
dip <dmytro.piskun@gmail.com>
2017-01-13 01:41:39 +0200

    Added request manager.

---
+++ Backup.Client.BL/WebAPIRequestsManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace Backup.Client.BL
+{
+    internal class WebAPIRequestsManager
+    {
+        private const string APP_PATH = "http://localhost:55122";
+        private static string token;
+
+        internal static void RegisterNewUser(string userName, string password)
+        {
+            var registerResult = Register(userName, password);
+            Dictionary<string, string> tokenDictionary = GetTokenDictionary(userName, password);
+            token = tokenDictionary["access_token"];
+            string userInfo = GetUserInfo(token);
+            string values = GetValues(token);
+        }
+
+        internal static string Register(string email, string password)
+        {
+            var registerModel = new
+            {
+                Email = email,
+                Password = password,
+                ConfirmPassword = password
+            };
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsJsonAsync(APP_PATH + "/api/Account/Register", registerModel).Result;
+                return response.StatusCode.ToString();
+            }
+        }
+
+        private static Dictionary<string, string> GetTokenDictionary(string userName, string password)
+        {
+            var pairs = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string, string>( "grant_type", "password" ),
+                    new KeyValuePair<string, string>( "username", userName ),
+                    new KeyValuePair<string, string> ( "Password", password )
+                };
+            var content = new FormUrlEncodedContent(pairs);
+
+            using (var client = new HttpClient())
+            {
+                var response =
+                    client.PostAsync(APP_PATH + "/Token", content).Result;
+                var result = response.Content.ReadAsStringAsync().Result;
+                Dictionary<string, string> tokenDictionary =
+                    JsonConvert.DeserializeObject<Dictionary<string, string>>(result);
+                return tokenDictionary;
+            }
+        }
+
+        internal static HttpClient CreateClient(string accessToken = "")
+        {
+            var client = new HttpClient();
+            if (!string.IsNullOrWhiteSpace(accessToken))
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", accessToken);
+            }
+            return client;
+        }
+
+        private static string GetUserInfo(string token)
+        {
+            using (var client = CreateClient(token))
+            {
+                var response = client.GetAsync(APP_PATH + "/api/Account/UserInfo").Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+        private static string GetValues(string token)
+        {
+            using (var client = CreateClient(token))
+            {
+                var response = client.GetAsync(APP_PATH + "/api/values").Result;
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+    }
+}
